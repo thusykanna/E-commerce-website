fb2326685ce00488a52116f599a527cf58744aad
thusykanna <thusy0816@gmail.com>
2025-03-25 01:40:41 +0530

    Add OurPolicy component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/latestCollection'
 import BestSellers from '../components/BestSellers'
+import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />  
       <LatestCollection />
       <BestSellers/>
+      <OurPolicy/>
     </div>
   )
 }
