eefe9946a51edde5a7fcd4d4e7633487a258d9ae
thusykanna <thusy0816@gmail.com>
2025-03-25 01:41:15 +0530

    Add return policy and customer support sections

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -11,6 +11,18 @@ const OurPolicy = () => {
         <p className='text-gray-400'>No offer hassle free exchange policy</p>
     </div>
 
+    <div>
+        <img src = {assets.quality_icon} className='w-12 m-auto mb-5' alt=''/>
+        <p className='font-semibold'>7 Days return Policy</p>
+        <p className='text-gray-400'>We provide 7 days free return policy</p>
+    </div>
+
+    <div>
+        <img src = {assets.support_img} className='w-12 m-auto mb-5' alt=''/>
+        <p className='font-semibold'>Best Customer Support</p>
+        <p className='text-gray-400'>We provide 24/7 customer support</p>
+    </div>
+
     </div>
   )
 }
