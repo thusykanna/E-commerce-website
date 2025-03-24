f9120c1e38cbfd9ef0dd69fc15c7ee1d018497ee
thusykanna <thusy0816@gmail.com>
2025-03-24 23:59:33 +0530

    Add Title and ProductItem components to BestSeller.jsx, and include BestSeller component in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/latestCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero />  
       <LatestCollection />
+      <BestSeller/>
     </div>
   )
 }
