213ae984cc18de73055cb5badb6e3dddb1000e4a
thusykanna <thusy0816@gmail.com>
2025-03-26 01:17:35 +0530

    Add Footer component and fix onSubmitHandler typo

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -27,6 +28,8 @@ const App = () => {
         <Route path='/Orders' element={<Orders/>} />
         <Route path='/Navbar' element={<Navbar/>} />
       </Routes>
+
+      <Footer />
 
     </div>
   )
--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 const NewsletterBox = () => {
+
+    const onSubmitHandler = (event) => {
+        event.preventDeafault();
+    }
+
   return (
     <div className='text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subscribe Now & Get 20% off</p>
       <p className='text-gray-400 mt-3'>Lorem epsum is simply a dummy text</p>
-      <form className='w-full sm-w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+      <form onSubmit={onSubmitHandler} className='w-full sm-w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your Email' required/>
         <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
       </form>
