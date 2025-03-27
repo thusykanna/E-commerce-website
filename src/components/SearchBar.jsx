dd0f814be5fe898af5391e76ec2ea8772b3141ae
thusykanna <thusy0816@gmail.com>
2025-03-27 12:47:03 +0530

    Add useState and useEffect hooks to SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import { useLocation } from 'react-router-dom';
@@ -6,20 +6,20 @@ import { useLocation } from 'react-router-dom';
 const SearchBar = () => {
 
     const {search, setSearch, showSearch, setShowSearch} = useContext(ShopContext);
-    // const location = useLocation();
-    // const [visible, setVisible] = useState(false);
+    const location = useLocation();
+    const [visible, setVisible] = useState(false);
 
-    // useEffect(() => {
-    //     console.log(location.pathname);
-    //     if (location.pathname.includes('Collections')) {
-    //       setVisible(true);
-    //     }
-    //     else {
-    //       setVisible(false);
-    //     }
-    // },[location])
+    useEffect(() => {
+        console.log(location.pathname);
+        if (location.pathname.includes('Collections')) {
+          setVisible(true);
+        }
+        else {
+          setVisible(false);
+        }
+    },[location])
 
-  return showSearch ? (
+  return showSearch && visible ? (
     <div className='border-t border-b bg-gray-75 text-center'>
       <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
         <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='search' />
