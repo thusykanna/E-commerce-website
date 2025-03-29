c235063b70a89ac3234d83a165b77a96867a648c
thusykanna <thusy0816@gmail.com>
2025-03-29 21:51:47 +0530

    Update search functionality in ShopContext and Collections

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collections = () => {
 
-  const {products} = useContext(ShopContext);
+  const {products, search, showSearch} = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -32,6 +32,11 @@ const Collections = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
     if(category.length > 0){
       productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
@@ -47,13 +52,8 @@ const Collections = () => {
   },[])
 
   useEffect(() => {
-    console.log(subCategory);
-  },[subCategory])
-
-
-  useEffect(() => {
     applyFilter();
-  },[category,subCategory])
+  },[category, subCategory, search, showSearch])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
