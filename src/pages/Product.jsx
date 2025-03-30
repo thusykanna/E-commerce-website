f203962c32b6984505e8f0f05a4641a1fa76c33a
thusykanna <thusy0816@gmail.com>
2025-03-31 00:54:00 +0530

    Add size selection to Product component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -10,6 +10,7 @@ const Product = () => {
   const {products, currency} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
+  const [size, setSize] = useState('');
 
   const fetchProductData = async() => {
     products.map((item) => {
@@ -62,7 +63,9 @@ const Product = () => {
               <div className='flex flex-col gap-4 my-8'>
                 <p>Select Size</p>
                 <div className='flex gap-2'>
-                  
+                  {productData.sizes.map((item, index) => (
+                    <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ""}`} key={index}>{item}</button>
+                  ))}
                 </div>
               </div>
           </div>
