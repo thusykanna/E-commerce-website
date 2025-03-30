152cf1765fdc0ca9c49c0081643c654642067404
thusykanna <thusy0816@gmail.com>
2025-03-31 01:12:30 +0530

    Add "ADD TO CART" button and description section

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -68,9 +68,28 @@ const Product = () => {
                   ))}
                 </div>
               </div>
+              <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+              <hr className='mt-8 sm:w-4/5' />
+              <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                    <p>100% Original product.</p>
+                    <p>Cash on delivery is available on this product</p>
+                    <p>Easy return and exchange policy within 7 days</p>
+              </div>
           </div>
-
       </div>
+
+      {/* Description and review section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
+          <p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
+        </div>
+      </div>
+
     </div>
   ) : <div className='opacity-0'>Loading...</div>
 }
