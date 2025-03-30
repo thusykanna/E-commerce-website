a311da7d6152404fa2ed463065d4dfdfd2c7357e
thusykanna <thusy0816@gmail.com>
2025-03-30 23:59:25 +0530

    Add image state and display product images

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,12 +8,13 @@ const Product = () => {
   const {productId} = useParams();
   const {products} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('');
 
   const fetchProductData = async() => {
     products.map((item) => {
       if(item._id === productId){
         setProductData(item);
-        console.log(item);
+        setImage(item.image[0]);
         return null;
       }
     })
@@ -23,11 +24,21 @@ const Product = () => {
     fetchProductData();
   },[productId, products])
 
-  return (
-    <div>
-      
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        {/* Product images */}
+
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+
+          </div>
+
+        </div>
+      </div>
     </div>
-  )
+  ) : <div className='opacity-0'>Loading...</div>
 }
 
 export default Product
