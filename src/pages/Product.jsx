4861c658f5936f04da265d30297bcc58fe096017
thusykanna <thusy0816@gmail.com>
2025-03-31 00:15:34 +0530

    Add image gallery and product name display

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -15,6 +15,7 @@ const Product = () => {
       if(item._id === productId){
         setProductData(item);
         setImage(item.image[0]);
+        console.log(item);
         return null;
       }
     })
@@ -32,10 +33,23 @@ const Product = () => {
 
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img onClick = {() => setImage(item)} src = {item} key = {index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
+          </div>
+            <div className='w-full sm:w-[80%]'>
+              <img className='w-full h-auto' src={image} alt="" />
+            </div>
+        </div>
+          
+          {/* Product details */}
 
+          <div className='flex-1'>
+              <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
           </div>
 
-        </div>
       </div>
     </div>
   ) : <div className='opacity-0'>Loading...</div>
