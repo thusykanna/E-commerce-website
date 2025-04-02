d639cc1b34ca4c965d6ba00b5b3a497e28f1fbe4
thusykanna <thusy0816@gmail.com>
2025-04-02 23:06:56 +0530

    Refactor Cart component to display items in the cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
+import { products } from '../assets/assets';
 
 const Cart = () => {
+
+  const {productsData, currency, cartItems} = useContext(ShopContext);
+  
+  const [cartdata, setCardData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]){
+        if (cartItems[items][item] > 0){
+          tempData.push({
+            _id : items ,
+            size : item,
+            quantity : cartItems[items][item],
+          });
+        }
+      }
+    }
+    // console.log(tempData);
+    setCardData(tempData);
+  }, [cartItems])
+
   return (
-    <div>
+    <div className='border-t pt-14'>
+
+      <div className='text-2xl mb-3'>
+        <Title text1 = {'Your'} text2 = {'Cart'} />
+      </div>
+
+      <div>
+        {
+          cartdata.map((item, index) => {
+
+            const productData = products.find((product) => product._id === item._id);
+
+            return (
+              <div key = {index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                  </div>
+                </div>
+              </div>
+            )
+
+          })
+        }
+      </div>
       
     </div>
   )
