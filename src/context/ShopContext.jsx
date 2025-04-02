d947d2b672456e7d2e9477370a2aed3d0e8d7fb0
thusykanna <thusy0816@gmail.com>
2025-04-02 23:28:08 +0530

    Add updateQuantity function to ShopContext and Cart page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -55,6 +55,15 @@ const ShopContextProvider = (props) => {
         return totalCount;
     }
 
+
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
+
     // useEffect(() => {
     //     console.log(cartItems);
     // },[cartItems])
@@ -69,7 +78,8 @@ const ShopContextProvider = (props) => {
         setShowSearch,
         cartItems,
         addToCart,
-        getCartCount
+        getCartCount,
+        updateQuantity
     }
 
     return (
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
-import { products } from '../assets/assets';
+import { assets, products } from '../assets/assets';
 
 const Cart = () => {
 
-  const {productsData, currency, cartItems} = useContext(ShopContext);
+  const {productsData, currency, cartItems, updateQuantity} = useContext(ShopContext);
   
   const [cartdata, setCardData] = useState([]);
 
@@ -45,8 +45,14 @@ const Cart = () => {
                   <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
                   <div>
                     <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                    <div className='flex items-center gap-5 mt-2'>
+                      <p>{currency}{productData.price}</p>
+                      <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                    </div>
                   </div>
                 </div>
+                <input className='border msx-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min = {1} defaultValue={item.quantity} />
+                <img onClick={() => updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
               </div>
             )
 
