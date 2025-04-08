f2a6664655952dd197ac8160f439a640287e3a52
thusykanna <thusy0816@gmail.com>
2025-04-09 01:42:47 +0530

    Folder changed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Nulla quam velit,
+                vulputate eu pharetra nec,
+                mattis ac neque.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+94 76 777 8888</li>
+                <li>contact@bestchoice.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+      <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2025@ bestchoice.com - All Rigths Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/latestCollection'
+import BestSellers from '../components/BestSellers'
+import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />  
+      <LatestCollection />
+      <BestSellers/>
+      <OurPolicy/>
+      <NewsletterBox/>
+    </div>
+  )
+}
+
+export default Home
