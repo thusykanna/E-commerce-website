a44313f5b687cc9922b36b55e759cf404563d142
thusykanna <thusy0816@gmail.com>
2025-04-10 23:55:05 +0530

    Add product upload functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,24 @@
 
 // function for add products
 const addProduct = async (req, res) => {
+    try {
+        
+        const { name, description, price, category, subCategory, sizes, bestseller} = req.body;
 
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        console.log(name, description, price, category, subCategory, sizes, bestseller);
+        console.log(image1, image2, image3, image4);
+
+        res.json({});
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 // function for list products
