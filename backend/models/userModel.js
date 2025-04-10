10117e9f0fbd06b5e93a08a5e630acf37aba0157
thusykanna <thusy0816@gmail.com>
2025-04-10 17:25:43 +0530

    Add Cloudinary configuration

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    cartData: {
+        type: Object,
+        default: {}
+    }
+}, {minimize: false})
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
