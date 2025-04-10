f186ff7d88458bfc92d00802cdd0ce0800cb5897
thusykanna <thusy0816@gmail.com>
2025-04-11 01:04:14 +0530

    Add product upload functionality with Cloudinary integration

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
-
+import {v2 as cloudinary} from 'cloudinary';
+import productModel from '../models/productModel.js';
 
 // function for add products
 const addProduct = async (req, res) => {
@@ -11,10 +12,33 @@ const addProduct = async (req, res) => {
         const image3 = req.files.image3 && req.files.image3[0];
         const image4 = req.files.image4 && req.files.image4[0];
 
-        console.log(name, description, price, category, subCategory, sizes, bestseller);
-        console.log(image1, image2, image3, image4);
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+        
+        const imageUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, {resource_type: "image"});
+                return result.secure_url;
+            })
+        )
 
-        res.json({});
+        const productData = {
+            name,
+            description,
+            price: Number(price),
+            image: imageUrl,
+            category,
+            subCategory,
+            sizes: JSON.parse(sizes),
+            bestseller: bestseller === "true" ? true : false,
+            date: Date.now(),
+        }
+
+        console.log(productData);
+
+        const product= new productModel(productData);
+        await product.save();
+
+        res.json({success: true, message: "Product added successfully"});
 
     } catch (error) {
         console.log(error);
