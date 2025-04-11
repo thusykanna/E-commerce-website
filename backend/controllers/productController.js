75fee6faa06302039fd15ce547060e8526cc42f5
thusykanna <thusy0816@gmail.com>
2025-04-11 15:10:50 +0530

    Refactor singleProduct function to handle errors

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -72,7 +72,14 @@ const removeProduct = async (req, res) => {
 
 // function for single product info
 const singleProduct = async (req, res) => {
-    
+    try {
+        const {productId} = req.body;
+        const product = await productModel.findById(productId);
+        res.json({ success: true, product });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 export { addProduct, listProduct, removeProduct, singleProduct }
