1a21eb3957a57f033725ada5b7eeb014cf74f1e6
thusykanna <thusy0816@gmail.com>
2025-04-11 15:47:01 +0530

    Add admin login functionality and update product routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,7 +85,21 @@ const registerUser = async (req, res) => {
 
 // Route to admin login
 const adminLogin = async (req, res) => {
-
+    try {
+        
+        const {email, password} = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET);
+            res.json({success: true, token });
+            
+        }
+        else {
+            res.json({success: false, message: "Invalid credentials" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message });
+    }
 }
 
 export { loginUser, registerUser, adminLogin }
