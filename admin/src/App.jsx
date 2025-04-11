9dfac983a64b380f731d069796fbdbb8ef1e2fd1
thusykanna <thusy0816@gmail.com>
2025-04-11 17:39:57 +0530

    Update App component and assets

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import './index.css'
+import Sidebar from './components/Sidebar.jsx'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-gray-50 min-h-screen'>
+    <>
+
+      <Navbar/>
+      <hr />
+      <div className='flex w-full'>
+        <Sidebar />
+      </div>
+    
+    </>
     </div>
   )
 }
 
 export default App
+
--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server :{port: 5174},
+  plugins: [react(),
+    tailwindcss(),],
+    server: {port: 5174},
 })
