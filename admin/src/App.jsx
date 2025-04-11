8366c63d169ad3211cf6d53b64fef86421d88ff1
thusykanna <thusy0816@gmail.com>
2025-04-11 17:46:55 +0530

    Add routes and pages for Add, List, and Orders

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import './index.css'
 import Sidebar from './components/Sidebar.jsx'
+import {Routes, Route} from 'react-router-dom'
+import Add from './pages/Add.jsx'
+import List from './pages/List.jsx'
+import Orders from './pages/Orders.jsx'
 
 const App = () => {
   return (
@@ -12,6 +16,13 @@ const App = () => {
       <hr />
       <div className='flex w-full'>
         <Sidebar />
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+          <Routes>
+            <Route path='/add' element={<Add/>} />
+            <Route path='/list' element={<List />} />
+            <Route path='/orders' element={<Orders />} />
+          </Routes>
+        </div>
       </div>
     
     </>
