91563e8f2a0a4fc113c1dedf021613f307bdf28a
thusykanna <thusy0816@gmail.com>
2025-04-12 23:36:06 +0530

    Add token persistence and logout functionality

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import './index.css'
 import Sidebar from './components/Sidebar.jsx'
@@ -7,25 +7,34 @@ import Add from './pages/Add.jsx'
 import List from './pages/List.jsx'
 import Orders from './pages/Orders.jsx'
 import Login from './components/Login.jsx'
+import { ToastContainer} from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
 
-  const [token, setToken] = useState('')
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : "");
+
+  useEffect(() => {
+      localStorage.setItem('token', token);
+  },[token])
 
   return (
     <div className='bg-gray-50 min-h-screen'>
-      {token === "" ? <Login /> :
+      <ToastContainer />
+      {token === "" ? <Login setToken={setToken}/> :
     <>
 
-      <Navbar/>
+      <Navbar setToken={setToken}/>
       <hr />
       <div className='flex w-full'>
         <Sidebar />
         <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
           <Routes>
-            <Route path='/add' element={<Add/>} />
-            <Route path='/list' element={<List />} />
-            <Route path='/orders' element={<Orders />} />
+            <Route path='/add' element={<Add token={token}/>} />
+            <Route path='/list' element={<List token={token}/>} />
+            <Route path='/orders' element={<Orders token={token}/>} />
           </Routes>
         </div>
       </div>
