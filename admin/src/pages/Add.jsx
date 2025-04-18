d21a8dbdb16364d5a269b10e373047e68ccf76ae
thusykanna <thusy0816@gmail.com>
2025-04-18 23:45:49 +0530

    Update styling and add form fields in Add.jsx

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Add = () => {
+
+  const [image1,setImage1] = useState("false"); 
+  const [image2,setImage2] = useState("false");                
+  const [image3,setImage3] = useState("false");
+  const [image4,setImage4] = useState("false");               
+
   return (
     <form className='flex flex-col w-full items-start gap-3'>
 
@@ -26,9 +32,78 @@ const Add = () => {
           </label>
         </div>
       </div>
-      <div>
-        <p>Product Name</p>
+      <div className='w-full'>
+        <p className='mb-2'>Product Name</p>
         <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required />
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Product description</p>
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required />
+      </div>
+
+      <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+
+        <div>
+          <p className='mb-2'>Product category</p>
+          <select className='w-full px-3 py-2'>
+            <option value="Men">Men</option>
+            <option value="Women">Women</option>
+            <option value="Kids">Kids</option>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>Sub category</p>
+          <select className='w-full px-2 py-3'>
+            <option value="Topwear">Topwear</option>
+            <option value="Bottomwear">Bottomwear</option>
+            <option value="Winterwear">Winterwear</option>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>product Price</p>
+          <input className='w-full px-3 py-2 sm:w-[120px]' type="Number" placeholder='25'/>
+        </div>
+        </div>
+
+        <div>
+          <div>
+            <p className='mb-2'>Product Sizes</p>
+            <div className='flex gap-3'>
+
+              <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+              </div>
+
+              <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+              </div>
+
+              <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+              </div>
+
+              <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+              </div>
+
+              <div>
+                <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+              </div>
+
+            </div>
+          
+        </div>
+
+        <div className='flex gap-2 mt-2'>
+          <input type="checkbox" id="bestseller"/>
+          <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+        </div>
+
+        <button type='submit' className='w-28 py-3 mt-4 bg-black text-white'>Add</button>
+
       </div>
     </form>
   )
