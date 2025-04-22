ef6a40cf20e0d7cf72b1e4833a6b6a247fa01161
thusykanna <thusy0816@gmail.com>
2025-04-22 23:27:53 +0530

    Add toast notifications for successful and failed responses

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios';
 import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
 
 const Add = ({token}) => {
 
@@ -42,8 +43,23 @@ const Add = ({token}) => {
 
       console.log(response.data);
 
+      if (response.data.success){
+        toast.success(response.data.message);
+        setName("");
+        setDescription("");
+        setImage1(false);
+        setImage2(false);
+        setImage3(false);
+        setImage4(false);
+        setPrice("");
+      }
+      else{
+        toast.error(response.data);
+      }
+
     } catch (error) {
-      console.error("Error occurred:", error);
+      console.log(error);
+      toast.error(error.message);
     }
   }
 
