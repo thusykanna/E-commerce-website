4dd22cc40e88972552c3b0e2991836d7be90e306
thusykanna <thusy0816@gmail.com>
2025-04-25 17:31:51 +0530

    Refactor Navbar component and add logout functionality

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -107,6 +107,12 @@ const ShopContextProvider = (props) => {
         getProductsData();
     },[])
 
+    useEffect(() => {
+        if (!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'));
+        }
+    },[])
+
     const value = {
         products,
         currency,
