3668372736483a5ba4e65e04b9ea80630d55e329
thusykanna <thusy0816@gmail.com>
2025-09-20 18:49:39 +0530

    fix: Correct import path for LatestCollection component to match filename casing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import LatestCollection from '../components/latestCollection'
+import LatestCollection from '../components/LatestCollection'
 import BestSellers from '../components/BestSellers'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
